646cec0c8db63267a1662348853d95c7a41d8a53
WesleyFransin <wesley.matheusf@gmail.com>
2024-03-30 11:02:10 -0300

    Feat: change descent direction calc to matrix-based

--- multivariable.py
+++ multivariable.py
@@ -18,18 +18,20 @@ def is_negative(x):
 def is_semi_negative(x):
     return np.all(np.linalg.eigvals(x) <= 0)
 
-def multivariable_search(f, gradient_f, x_0, calculate_d, find_alpha, precision, **kwargs):
+def multivariable_search(f, gradient_f, x_0, calculate_D_matrix, find_alpha, precision, **kwargs):
     i = 0
     stop_criteria = False
     
     x_i = x_0
     x_last = None
     g_last = None
+    D_last = None 
     while(i < MAX_ITERATIONS and not stop_criteria):
         f_i = f(x_i)
         g_i = gradient_f(x_i)
 
-        d_i = calculate_d(g_i, x=x_i, x_last=x_last, g_last=g_last, **kwargs)
+        D_i = calculate_D_matrix(g_i, x=x_i, x_last=x_last, g_last=g_last, D_last = D_last, **kwargs)
+        d_i = np.linalg.solve(D_i, -g_i)
         d_i = d_i/np.linalg.norm(d_i)
         int_prod_gd = np.inner(np.transpose(g_i), d_i)
 
@@ -40,6 +42,9 @@ def multivariable_search(f, gradient_f, x_0, calculate_d, find_alpha, precision,
 
         # α = scipy.optimize.minimize_scalar(f_alpha).x
         α = find_alpha(f_alpha, **kwargs)
+        
+        D_last = D_i
+        g_last = g_i
         x_last = x_i
         x_i = x_i + α*d_i
 
@@ -50,12 +55,23 @@ def multivariable_search(f, gradient_f, x_0, calculate_d, find_alpha, precision,
     return x_i
 
 def d_gradient_method(g, **kwargs):
-    return -g
+    return np.identity(len(g))
 
 def d_newton_method(g, hessian, x, **kwargs):
-    h = hessian(x)
-    d = np.linalg.solve(h, -g)
-    return d
+    return hessian(x)
+
+def d_quasi_newton(g, g_last, x, x_last, D_last, **kwargs):
+    if(type(g_last) != np.ndarray  or type(x_last) != np.ndarray  or type(D_last) != np.ndarray):
+        return np.identity(len(g))
+
+    s = x - x_last
+    y = g - g_last
+
+    if(np.transpose(s).dot(y) < 0):
+        return np.identity(len(g))
+    
+    D = D_last + s.dot(np.transpose(s)) * np.transpose(s).dot(y) - D_last.dot(y) * np.transpose(y).dot(D_last)/np.transpose(y).dot(D_last).dot(y)
+    return D
 
 def f_1(x):
     y = 10*(x[1] - x[0]**2)**2 + (1-x[0])**2
@@ -80,13 +96,13 @@ def h_f1(x):
 
 scipy_search = lambda f, **kwargs : scipy.optimize.minimize_scalar(f).x
 search_function = bisection_search # bisection_search, scipy_search
-direction_method = d_gradient_method # d_gradient_method, d_newton_method
+direction_method = d_gradient_method # d_gradient_method, d_newton_method, d_quasi_newton
 
 result = multivariable_search(f_1, g_f1, [-1.2, 1],
                                 direction_method, search_function,
                                 precision=1e-6, max_iterations=1000,
                                 linear_precision=1e-7, linear_max_iterations=1000,
-                                search_interval=[0, 5], uncertainty_distance=1e-8,
+                                search_interval=[0, 3], uncertainty_distance=1e-8,
                                 hessian=h_f1
                                 )
 print(result)
