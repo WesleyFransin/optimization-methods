2c32732bb23ed55740b78a86a7b031113ca12516
WesleyFransin <wesley.matheusf@gmail.com>
2024-06-14 08:02:42 -0300

    Feat: add "phase 2" simplex, to find initial base

--- simplex.py
+++ simplex.py
@@ -5,21 +5,25 @@ def get_non_basic_variables(x, I):
     mask[I] = False
     return np.arange(len(x))[mask]
 
-def pivot_system(A, b, c, s, r):
-
-    return A_new, b_new, c_new
-
-def simplex(c, A, b, find_base=False):
+def simplex(A, b, c, find_base=False):
     MAX_ITERATIONS = 10
     A = np.array(A)
     b = np.array(b)
     c = np.array(c)
-    x = np.zeros(A.shape[1])
+    base_variables_I = np.array([])
 
-    # TODO: escolher variáveis básicas iniciais
-    # base_variables_I = np.array([2, 3, 4])
-    base_variables_I = np.array([1, 3, 4])
-    non_base_variables_J = get_non_basic_variables(x, base_variables_I)
+    # Encontrar solução básica factível de partida
+    if(find_base):
+        n_artificial_var = A.shape[0] 
+        A = np.append(A, np.identity(n_artificial_var), axis=1)
+        c = np.zeros(len(c) + n_artificial_var)
+        c[-n_artificial_var:] = 1
+        base_variables_I,  = np.where(c == 1)
+    else:
+        x_artificial = simplex(A, b, c, find_base=True)
+        base_variables_I,  = np.where(x_artificial != 0)
+
+    non_base_variables_J = get_non_basic_variables(c, base_variables_I)
     i = 0
     while i < MAX_ITERATIONS:
         i += 1
@@ -31,11 +35,9 @@ def simplex(c, A, b, find_base=False):
 
         A_i_inv = np.linalg.inv(A_i)
         π_t = np.matmul(c_i.T, A_i_inv)
-        b_hat = np.matmul(A_i_inv, b)
 
         c_j_hat = c_j - np.matmul(π_t, A_j) # novos coeficientes da função
         b_hat = np.matmul(A_i_inv, b)
-        A_j_hat =  np.matmul(A_i_inv, A_j)
         A_hat = np.matmul(A_i_inv, A)
 
         if( (not find_base and not np.any(c_j_hat > 0))
@@ -65,12 +67,13 @@ def simplex(c, A, b, find_base=False):
 
         A = A_hat
         b = b_hat
-        c = np.zeros(len(x))
+        c = np.zeros(len(c))
         c[non_base_variables_J] = c_j_hat
         
         base_variables_I = np.delete(base_variables_I, r_min)
         base_variables_I = np.append(base_variables_I, new_basic_variable_index)
-        non_base_variables_J = get_non_basic_variables(x, base_variables_I)
+        non_base_variables_J = get_non_basic_variables(c, base_variables_I)
+    x = np.zeros(len(c))
     x[base_variables_I] = np.linalg.solve(A_hat[:, base_variables_I], b_hat)
     return x
 
@@ -78,6 +81,7 @@ def simplex(c, A, b, find_base=False):
  max z = c.T * x
  s.a. A*x = b
 '''
+# Forma padrão
 c = [-1, 2, 0, 0, 0] # Coeficientes da função
 A = [ # Coeficientes das restrições
     [1, 1, -1, 0, 0],
@@ -90,24 +94,6 @@ b = [ # Valores das restrições
     3
 ]
 
-###############
-c = [-1, 2, 0, 0, 0] # Coeficientes da função
-A = [ # Coeficientes das restrições
-    [1, 1, -1, 0, 0],
-    [-1, 1,  0, -1, 0],
-    [0, 1, 0, 0, 1]
-]
-b = [ # Valores das restrições
-    2,
-    1,
-    3
-]
-
-
-result = simplex(c, A, b)
-
-print(f'z = {np.matmul(c, result)} / x = {result}')
-
-result = simplex(c, A, b, find_base=True)
+result = simplex(A, b, c)
 
 print(f'z = {np.matmul(c, result)} / x = {result}')
